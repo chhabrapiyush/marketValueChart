9c303d07832af0e5af51e48c7fedc8c8363d7629
Piyush Chhabra <piyushchhabra.mobile@gmail.com>
2025-04-03 21:10:46 -0400

    Initial Commit

---
+++ MarketValueChart/MarketValueChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketValueChartApp.swift
+//  MarketValueChart
+//
+//  Created by Piyush Chhabra on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketValueChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarketValueChartTests/MarketValueChartTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketValueChartTests.swift
+//  MarketValueChartTests
+//
+//  Created by Piyush Chhabra on 4/3/25.
+//
+
+import Testing
+@testable import MarketValueChart
+
+struct MarketValueChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
